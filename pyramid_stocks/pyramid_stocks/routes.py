606bcaabbd75239a3ed5b700b8183a0ee74b05bb
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-05 23:30:41 -0700

    almost finished lab

--- pyramid_stocks/pyramid_stocks/routes.py
+++ pyramid_stocks/pyramid_stocks/routes.py
@@ -1,7 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/index')
-    config.add_route('auth', '/register')
+    config.add_route('home', '/')
+    config.add_route('auth', '/auth')
     config.add_route('stock', '/stock_add')
     config.add_route('portfolio', '/portfolio')
     config.add_route('portfolio/{symbol}', '/stock_detail')
