65b81da59487643b1d2c7405a11068de3c680d01
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-05 22:29:04 -0700

    fixing the routes

---
+++ pyramid_stocks/pyramid_stocks/sample_data/__init__.py
@@ -0,0 +1,110 @@
+MOCK_DATA = {
+    {
+    "symbol": "GE",
+    "companyName": "General Electric Company",
+    "exchange": "New York Stock Exchange",
+    "industry": "Industrial Products",
+    "website": "http://www.ge.com",
+    "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+    }, 
+    {
+    "symbol": "AAPL",
+    "companyName": "Apple Inc",
+    "exchange": "NASDAQ",
+    "industry": "Telecommunications Equipment",
+    "website": "www.apple.com",
+    "description": "Apple, Inc. engages in the design, manufacture, and marketing of mobile communication, media devices, personal computers, and portable digital music players. It operates through the following geographical segments: Americas, Europe, Greater China, Japan, and Rest of Asia Pacific. The Americas segment includes both North and South America. The Europe segment consists of European countries, as well as India, the Middle East, and Africa. The Greater China segment comprises of China, Hong Kong, and Taiwan. The Rest of Asia Pacific segment includes Australia and Asian countries not included in the reportable operating segments of the company. The company was founded by Steven Paul Jobs, Ronald Gerald Wayne, and Stephen G. Wozniak on April 1, 1976 and is headquartered in Cupertino, CA.",
+    "CEO": "Tim Cook",
+    "issueType": "cs",
+    "sector": "Electronic Technology"
+    },
+    {
+    "symbol": "C",
+    "companyName": "Citigroup Inc",
+    "exchange": "New York Stock Exchange",
+    "industry": "Financial Conglomerates",
+    "website": "www.citigroup.com",
+    "description": "Citigroup, Inc. is a holding company, which engages in the provision of financial products and services. It operates through the following segments: Global Consumer Banking; Institutional Clients Group; and Corporate and Other. The Global Consumer Banking segment includes global, full-service consumer franchise, array of banking, including commercial banking, credit card lending, and investment. The Institutional Clients Group segment composes of banking and markets; and securities services. The Corporate and Other segment involves activities that are not assigned to a specific business segment as well as certain North America and international loan portfolios, other legacy assets, and discontinued operations. The company was founded on October 8, 1998 and is headquartered in New York, NY.",
+    "issueType": "cs",
+    "sector": "Finance"
+    },
+    {
+    "symbol": "GOOGL",
+    "companyName": "Alphabet Inc",
+    "exchange": "NASDAQ",
+    "industry": "Internet Software/Services",
+    "website": "www.abc.xyz",
+    "description": "Alphabet, Inc. is a holding company, which engages in the business of acquisition and operation of different companies. It operates through the Google and Other Bets segments. The Google segment includes its main Internet products such as Search, Ads, Commerce, Maps, YouTube, Apps, Cloud, Android, Chrome, Google Play as well as hardware products, such as Chromecast, Chromebooks and Nexus. The Other Bets segment includes businesses such as Access or Google Fiber, Calico, Nest, Verily, GV, Google Capital, X, and other initiatives. The company was founded by Lawrence E. Page and Sergey Mikhaylovich Brin on October 2, 2015 and is headquartered in Mountain View, CA.",
+    "CEO": "Lawrence E. Page",
+    "issueType": "cs",
+    "sector": "Technology Services"
+    }, 
+    {
+    "symbol": "MSFT",
+    "companyName": "Microsoft Corp",
+    "exchange": "NASDAQ",
+    "industry": "Packaged Software",
+    "website": "www.microsoft.com",
+    "description": "Microsoft Corp. engages in the provision of developing and marketing software and hardware services. Its products include operating systems for computing devices, servers, phones and intelligent devices. It also offers server applications for distributed computing environments, productivity applications, business solution applications, desktop and server management tools, software development tools, video games, and online advertising. It operates through the following segments: Productivity and Business Processes; Intelligent Cloud; and More Personal Computing. The Productivity and Business Processes segment consists of products and cloud services in portfolio of productivity, communication, and information services. It comprises of office commercial, office consumer, and microsoft dynamics business solutions. The Intelligent Cloud segment offers hybrid server products and cloud services. It comprises of server products and cloud services and enterprise services. The More Personal Computing segment comprises of windows, devices, gaming, and search advertising. The company was founded by William Henry Gates III in 1975 and is headquartered in Redmond, WA.",
+    "CEO": "Satya Nadella",
+    "issueType": "cs",
+    "sector": "Technology Services"
+    }, 
+    {
+    "symbol": "FB",
+    "companyName": "Facebook Inc",
+    "exchange": "NASDAQ",
+    "industry": "Internet Software/Services",
+    "website": "www.facebook.com",
+    "description": "Facebook, Inc. engages in the development of social media applications for people to connect through mobile devices, personal computers, and other surfaces. It enables users to share opinions, ideas, photos, videos, and other activities online. Its products include Facebook, Instagram, Messenger, WhatsApp, and Oculus. The company was founded by Mark Elliot Zuckerberg, Dustin Moskovitz, Chris R. Hughes, Andrew McCollum and Eduardo P. Saverin on February 4, 2004 and is headquartered in Menlo Park, CA.",
+    "CEO": "Mark Zuckerberg",
+    "issueType": "cs",
+    "sector": "Technology Services"
+    }, 
+    {
+    "symbol": "F",
+    "companyName": "Ford Motor Co",
+    "exchange": "New York Stock Exchange",
+    "industry": "Motor Vehicles",
+    "website": "www.ford.com",
+    "description": "Ford Motor Co. engages in the manufacture, distribution, and sale of automobiles. It operates through the following segments: Automotive and Financial Services. The Automotive segment engages in designing, manufacturing, marketing and servicing of Ford cars, trucks, SUVs, and electrified vehicles, as well as Lincoln luxury vehicles. The Financial Services segment consists of Ford Credit, which offers automotive financing products to and through automotive dealers. The company was founded by Henry Ford on June 16, 1903 and is headquartered in Dearborn, MI.",
+    "CEO": "James Hackett",
+    "issueType": "cs",
+    "sector": "Consumer Durables"
+    }, 
+    {
+    "symbol": "EQT",
+    "companyName": "EQT Corp",
+    "exchange": "New York Stock Exchange",
+    "industry": "Oil & Gas Production",
+    "website": "www.eqt.com",
+    "description": "EQT Corp. focuses on natural gas production, gathering and transmission in the Appalachian area. It operates its business through the following segments: EQT Production, EQT Gathering and EQT Transmission. The EQT Production segment engages in the exploration, development and production of natural gas, natural gas liquids and crude oil. The EQT Gathering segment contains the Company's gathering assets that are included in EQM. The EQT Transmission segment includes the company's FERC-regulated interstate pipeline and storage operations. The company was founded in 1888 and is headquartered in Pittsburgh, PA.",
+    "CEO": "David Porges",
+    "issueType": "cs",
+    "sector": "Energy Minerals"
+    }, 
+    {
+    "symbol": "REGN",
+    "companyName": "Regeneron Pharmaceuticals Inc",
+    "exchange": "NASDAQ",
+    "industry": "Biotechnology",
+    "website": "www.regeneron.com",
+    "description": "Regeneron Pharmaceuticals, Inc. operates as a biopharmaceutical company, which engages in inventing medicines for people with serious diseases. It includes Arcalyst, Dupixent, Eylea, Kevzara, and Praluent injections. The company was founded by Alferd G. Gilman, Leonard S. Schleifer, and Eric M. Shooter on January 8, 1988 and is headquartered in Tarrytown, NY.",
+    "CEO": "Leonard Schleifer",
+    "issueType": "cs",
+    "sector": "Health Technology"
+    }, 
+    {
+    "symbol": "UA",
+    "companyName": "Under Armour Inc",
+    "exchange": "New York Stock Exchange",
+    "industry": "Apparel/Footwear",
+    "website": "www.underarmour.com",
+    "description": "Under Armour, Inc. engages in the development, marketing, and distributiion of branded performance apparel, footwear, and accessories for men, women, and youth. It operates through the following segments: North America, EMEA, Asia-Pacific, Latin America, and Connected Fitness. The North America segment comprises of U.S. and Canada. The Connected Fitness segment offers digital fitness subscriptions, along with digital advertising through its MapMyFitness, MyFitnessPal and Endomondo. applications. The company was founded by Kevin A. Plank in 1996 and is headquartered in Baltimore, MD.
+    "issueType": "cs",
+    "sector": "Consumer Non-Durables"
+    }, 
+}
