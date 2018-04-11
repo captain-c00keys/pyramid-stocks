b73903cf26e457905280be53467af2e7857e0565
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-04-10 23:18:58 -0700

    almost finished this one

--- pyramid_stocks/pyramid_stocks/models/mymodel.py
+++ pyramid_stocks/pyramid_stocks/models/mymodel.py
@@ -1,8 +1,10 @@
+
 from sqlalchemy import (
     Column,
     Index,
     Integer,
     Text,
+    DateTime
 )
 
 from .meta import Base
@@ -11,8 +13,15 @@ from .meta import Base
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
+    symbol = Column(Text, nullable=False, unique=True)
+    companyName = Column(Text)
+    exchange = Column(Text)
+    website = Column(Text)
+    CEO = Column(Text)
+    industry = Column(Text)
+    sector = Column(Text)
+    issueType = Column(Text)
+    description = Column(Text)
+    
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
